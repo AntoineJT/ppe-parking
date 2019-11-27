87d6110c9df7156671cc2b41ce03f432a686c6bd
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-11-27 10:00:26 +0100

    :construction: Add lorem ipsum presentation page

--- src/main/routes/web.php
+++ src/main/routes/web.php
@@ -11,6 +11,60 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
+/*
+ * pagelist
+ *
+    /
+    /connexion
+    /deconnexion
+    /inscription
+    /recuperationcompte
+    /consultation
+    /consultation/places
+    /consultation/historique
+    /consultation/statut
+    /reservation/creer
+    /moncompte
+    /admin
+    /admin/connexion
+    /admin/gestion/utilisateurs
+    /admin/gestion/places
+    /admin/gestion/file
+
+ */
+
+function embedInMainView(string $content, bool $public, bool $admin)
+{
+    /*
+    if (session('compte') == NULL) {
+        return Redirect::to('login');
+    }
+    */
+    return view('main', [
+        'title' => 'Accueil',
+        'content' => $content,
+        'publicContent' => $public,
+        'admin' => $admin
+    ]);
+}
+
+function embedPageInMainView(string $path, bool $public, bool $admin) {
+    $content = view('pages/' . $path);
+    return embedInMainView($content, $public, $admin);
+}
+
+function declareSubpage(string $path, bool $public, bool $admin) {
+    Route::get($path, function() use ($admin, $public, $path) {
+        return embedPageInMainView($path, $public, $admin);
+    });
+}
+
+// TODO Replace with the good one
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return view('main', ['title' => 'Accueil', 'publicContent' => true, 'content' => '', 'admin' => false]);
 });
+
+declareSubPage('presentation', true, false);
