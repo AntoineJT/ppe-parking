5eb627233ac1ce87009c68f11fb74892ee648369
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-11-27 11:07:59 +0100

    :lipstick: Improve a bit "connexion" page UI

--- src/main/resources/views/pages/connexion.blade.php
+++ src/main/resources/views/pages/connexion.blade.php
@@ -1,15 +1,10 @@
-<form method="POST" class="form-signin">
+<form method="POST" class="form-signin p-2">
     @csrf
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <img class="mb-4" src="images/mdl.png" class="rounded float-right" alt="Responsive image">
-    <h1 class="h3 mb-3 font-weight-normal">Connexion</h1>
-    <p>
-        {{ $errors->first('etablissement') }}
-        {{ $errors->first('mdp') }}
-    </p>
+    <h1 class="center text-white mb-4 font-weight-normal">Connexion</h1>
     <label for="mail" class="sr-only">Adresse Mail</label>
-    <input type="text" name="mail" class="form-control" placeholder="Adresse Mail" required autofocus>
+    <input type="text" name="mail" class="form-control mb-1" placeholder="Adresse Mail" required autofocus>
     <label for="mdp" class="sr-only">Mot de Passe</label>
-    <input type="password" name="mdp" class="form-control" placeholder="Mot de Passe" required>
+    <input type="password" name="mdp" class="form-control mb-3" placeholder="Mot de Passe" required>
     <button class="btn btn-lg btn-primary btn-block" type="submit">Se Connecter</button>
 </form>
