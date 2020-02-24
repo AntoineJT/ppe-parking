7afa5504f26f777309590831f4ca5b15179c49bb
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-02-24 16:05:23 +0100

    Update 2020_02_24_145439_create_statut_table.php

--- src/main/database/migrations/2020_02_24_145439_create_statut_table.php
+++ src/main/database/migrations/2020_02_24_145439_create_statut_table.php
@@ -14,6 +14,10 @@ class CreateStatutTable extends Migration
     public function up()
     {
         Schema::create('statut', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            // $table->charset = 'utf8';
+            $table->collation = 'utf8mb4_unicode_ci';
+
             $table->tinyInteger('type_statut')->primary();
             // $table->timestamps();
             $table->string('nom_statut', 10)->unique();
