629192a9d78fed9277475508f2dfb3b871c2cfdc
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-02-24 16:08:00 +0100

    Create 2020_02_24_150647_create_utilisateur_table.php

---
+++ src/main/database/migrations/2020_02_24_150647_create_utilisateur_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUtilisateurTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('utilisateur', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            // $table->charset = 'utf8';
+            $table->collation = 'utf8mb4_unicode_ci';
+
+            $table->integerIncrements('id');
+            // $table->timestamps();
+            $table->string('mdp', 50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('utilisateur');
+    }
+}
