410bbbd63780a7a1e310c24686989e88c25beb56
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-02-24 15:34:12 +0100

    Create 2 migrations

---
+++ src/main/database/migrations/2020_02_24_141555_create_personnel_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonnelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personnel', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            // $table->charset = 'utf8';
+            $table->collation = 'utf8mb4_unicode_ci';
+
+            $table->bigIncrements('id');
+            $table->string('mail', 70);
+            $table->string('nom', 50);
+            $table->string('prenom', 50);
+            $table->tinyInteger('valide');
+            $table->integer('id_ligue');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personnel');
+    }
+}
