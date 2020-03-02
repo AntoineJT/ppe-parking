fd35a0c702b92bae0d0bc85361e58d9331479804
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-03-02 15:09:56 +0100

    :sparkles: Add a login page without controller
    FlashMessage feature is broken!

---
+++ src/app/Enums/AuthEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace App\Enums;
+
+
+class AuthEnum
+{
+    public const AUTH_ANIM = 'anim';
+    public const AUTH_ADMIN = 'admin';
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -11,6 +11,45 @@
 |
 */
 
+use App\Utils\FlashMessage;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Route;
+
+const ACCESS_PUBLIC = 0;
+const ACCESS_SEMIPUBLIC = 1;
+const ACCESS_ADMIN = 2;
+
+function embedInMainView(string $content, string $title, int $access_level) : string
+{
+    return view('main', [
+        'title' => $title,
+        'content' => $content,
+        'access' => $access_level
+    ]);
+}
+
+function declareSubpageByPath(string $path, string $title, int $access_level) : void {
+    declareSubpage($path, $path, $title, $access_level);
+}
+
+function declareSubpage(string $name, string $path, string $title, int $access_level) : void {
+    Route::get($name, function() use($title, $path, $access_level) {
+        $content = view("pages/$path");
+        return embedInMainView($content, $title, $access_level);
+    });
+}
+
+/*
 Route::get('/', function () {
     return view('welcome');
+});
+*/
+
+Route::redirect('/', '/connexion');
+declareSubpageByPath('/connexion', 'Connexion', ACCESS_PUBLIC);
+
+Route::get('/deconnexion', function() {
+    Request::session()->flush();
+    return FlashMessage::redirectWithInfoMessage(Redirect::to('/'), 'Vous vous êtes déconnecté!');
 });
