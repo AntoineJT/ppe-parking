2eddabb5bf2ba3d8af613271fd0beaa87d499224
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-03-07 14:48:40 +0100

    Finalize registration feature

--- src/resources/views/emails/resetlink.blade.php
+++ src/resources/views/emails/resetlink.blade.php
@@ -1,4 +1,5 @@
-{{--Faire un header stylé avec logo etc.--}}
-Vous avez demandé à réinitialiser votre mot de passe : <a href="{{ \Illuminate\Support\Facades\URL::to('/reinitialiser-mot-de-passe/') . $reset_link }}">{{ $url . $reset_link }}</a>
+{{-- Si STATE_NEWLY_CREATED alors spécifier qu'il devra attendre validation de l'admin pour se connecter --}}
+@php($url = \Illuminate\Support\Facades\URL::to("/reinitialiser-mot-de-passe/$reset_link"))
+Vous avez demandé à réinitialiser votre mot de passe : <a href="{{ $url }}">{{ $url }}</a>
 
 Ne faites rien si vous n'avez pas demandé cette réinitialisation, sinon cliquez sur le lien ci-dessus!
