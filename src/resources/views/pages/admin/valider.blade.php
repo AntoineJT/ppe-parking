37d56014c90c62b93e2a6f3e39a2aad2c5c75e44
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-03-09 16:46:15 +0100

    List users

--- src/resources/views/pages/admin/valider.blade.php
+++ src/resources/views/pages/admin/valider.blade.php
@@ -1,3 +1,6 @@
-@foreach(\App\Models\PersonnelModel::where('statut', '=', \App\Enums\UserStateEnum::STATE_DISABLED) as $user)
-    {{ $user->nom }}
-@endforeach
+<ul>
+    @foreach(\App\Models\PersonnelModel::where('statut', \App\Enums\UserStateEnum::STATE_DISABLED)->get() as $personnel)
+        @php($user = $personnel->getUser())
+        <li>{{ $user->nom }} {{ $user->prenom }}</li>
+    @endforeach
+</ul>
