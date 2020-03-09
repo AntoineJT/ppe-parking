06d02b4cccd1747bd3cf9c09a7b48c84b2db3a18
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-03-09 11:31:49 +0100

    Add forgotten seeders and migrations, remove default migrations

---
+++ src/database/migrations/2020_02_24_150246_create_reservation_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservation', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            // $table->charset = 'utf8';
+            $table->collation = 'utf8mb4_unicode_ci';
+
+            $table->integerIncrements('id_res');
+            // $table->timestamps();
+            $table->date('date_demande');
+            $table->date('date_debut');
+            $table->date('date_fin');
+            $table->integer('id_personnel')->unsigned();
+            $table->tinyInteger('type_statut')->unsigned();
+            $table->string('numero_place', 10);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservation');
+    }
+}
--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(StatutTableSeeder::class);
+
+        if (env('APP_DEBUG')) {
+            $this->call(UtilisateurTableSeeder::class);
+        }
     }
 }
