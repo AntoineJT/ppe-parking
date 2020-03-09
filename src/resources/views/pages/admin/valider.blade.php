71bc5424ec3732f5181c662116ab79c2b8a3425d
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-03-09 16:53:44 +0100

    Update valider.blade.php

--- src/resources/views/pages/admin/valider.blade.php
+++ src/resources/views/pages/admin/valider.blade.php
@@ -1,6 +1,13 @@
 <ul>
     @foreach(\App\Models\PersonnelModel::where('statut', \App\Enums\UserStateEnum::STATE_DISABLED)->get() as $personnel)
         @php($user = $personnel->getUser())
-        <li>{{ $user->nom }} {{ $user->prenom }}</li>
+        <li>
+            <form>
+                @csrf
+                {{ $user->nom }} {{ $user->prenom }}
+                <input type="hidden" name="id" value="{{ $user->id }}">
+                <button class="ml-2 btn btn-success"><i class="fas fa-check mr-2"></i>Valider!</button>
+            </form>
+        </li>
     @endforeach
 </ul>
