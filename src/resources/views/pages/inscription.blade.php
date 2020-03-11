f824243302b29d022ffebdb56fcfda3d8e38aded
MattDuc <55089890+MattDuc@users.noreply.github.com>
2020-03-11 11:32:28 +0100

    :lipstick: Work on registration and reset password pages design

--- src/resources/views/pages/inscription.blade.php
+++ src/resources/views/pages/inscription.blade.php
@@ -1,18 +1,23 @@
-<div class="d-flex justify-content-center text-center">
-    <form method="POST" class="p-4 form-group">
+<div class="register-form m-auto">
+    <form method="POST">
         @csrf
-        <p class="h1">Formulaire d'inscription</p>
+        <div class="text-center"><h3>Formulaire d'inscription</h3></div></br>
         <p>
-            <i class="fas fa-exclamation-triangle mr-2"></i>Il s'agit d'une plateforme interne!<br>
-            Un lien de réinitialisation vous permettra de choisir un mot de passe et de vérifier votre adresse mail.<br>
-            Votre compte sera désactivé jusqu'à ce que l'administrateur l'approuve!
-        </p>
-        <div class="d-flex flex-column container mt-3 mb-2">
-            <input name="nom" type="text" placeholder="Nom" class="form-control" required>
-            <input name="prenom" type="text" placeholder="Prénom" class="form-control" required>
-            <input name="courriel" type="email" placeholder="Adresse e-mail" class="form-control" required>
-            <button class="pl-3 pr-3 pb-1 pt-1 mt-2 btn btn-primary" type="submit">S'enregistrer!</button>
-        </div>
-        <a href="/connexion">Se connecter plutôt</a>
+            <i class="fas fa-exclamation-triangle mr-2"></i><i>Il s'agit d'une plateforme interne!<br>
+            Un mail vous sera envoyé avec un lien de réinitialisation vous permettra de choisir un mot de passe et de vérifier votre adresse mail.<br>
+            Votre compte sera désactivé jusqu'à ce que l'administrateur l'approuve !</i>
+        </p></br>
+          <div class="form-group">
+            <input class="form-control" name="nom" type="text" placeholder="Nom" class="form-control" required>
+          </div>
+          <div class="form-group">
+            <input class="form-control" name="prenom" type="text" placeholder="Prénom" class="form-control" required>
+          </div>
+          <div class="form-group">
+            <input class="form-control" name="courriel" type="email" placeholder="Adresse e-mail" class="form-control" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required>
+          </div></br>
+            <button class="btn-block btn btn-primary btn-b" type="submit">S'enregistrer</button></br>
+          <center><a href="/connexion"><i>Se connecter</i></a></center>
+
     </form>
 </div>
