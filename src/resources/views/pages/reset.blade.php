258b58e0dd21af46910f94a7d5d163086b88e57b
MattDuc <55089890+MattDuc@users.noreply.github.com>
2020-03-11 11:32:28 +0100

    :lipstick: Work on registration and reset password pages design

--- src/resources/views/pages/reset.blade.php
+++ src/resources/views/pages/reset.blade.php
@@ -1,14 +1,17 @@
-<div class="d-flex justify-content-center text-center">
+
+<div class="mdp-form m-auto">
     <form method="POST">
         @csrf
-        <p class="h1">Réinitialiser le mot de passe</p>
-        <p class="mb-4">Pour réinitialiser votre mot de passe, renseignez l'adresse de courriel avec laquelle vous avez
-            été inscrit.
-            <br>Si cette adresse e-mail est effectivement associée à un compte, vous recevrez un mail vous permettant
-            d'en changer le mot de passe.
-        </p>
-        <input type="email" name="courriel" placeholder="Adresse e-mail" required>
-        <button class="pl-3 pr-3 pb-1 pt-1 border-0 text-white bg-dark" type="submit">Réinitialiser le mot de passe
-        </button>
+        <div class="text-center"><h3>Réinitialiser le mot de passe</h3></div></br>
+        <p>
+            <i class="fas fa-exclamation-triangle mr-2"></i><i>Pour réinitialiser votre mot de passe, renseignez l'adresse de courriel avec laquelle vous avez
+                été inscrit.
+                <br>Si cette adresse e-mail est effectivement associée à un compte, vous recevrez un mail vous permettant
+                d'en changer le mot de passe.</i>
+        </p></br>
+          <div class="form-group">
+            <input class="form-control" name="courriel" type="email" placeholder="Votre e-mail" class="form-control" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
+          </div>
+          <button class="btn-block btn btn-primary btn-b" type="submit">Réinitialiser le mot de passe</button>
     </form>
 </div>
