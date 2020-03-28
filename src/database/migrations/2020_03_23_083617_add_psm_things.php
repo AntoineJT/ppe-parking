a281f218f0f1ee7052505359e417b36b97fa7353
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-03-28 17:38:53 +0100

    :sparkles: Update expired waiting reservations to refused state using PSM

--- src/database/migrations/2020_03_23_083617_add_psm_things.php
+++ src/database/migrations/2020_03_23_083617_add_psm_things.php
@@ -1,11 +1,8 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 
-// TODO Make WAITING expired reservations state = REFUSED
 class AddPsmThings extends Migration
 {
     /**
@@ -15,24 +12,40 @@ class AddPsmThings extends Migration
      */
     public function up()
     {
-        assert(DB::unprepared('DROP PROCEDURE IF EXISTS FAST_REFRESH_AVAILABILITIES'));
-        assert(DB::unprepared('
+        self::assertQuery('DROP PROCEDURE IF EXISTS REFRESH_AVAILABILITIES');
+        self::assertQuery('
+            CREATE PROCEDURE REFRESH_AVAILABILITIES()
+            BEGIN
+                -- Marque les réservations actives expirées comme expirées
+                UPDATE reservations SET type_statut = 1 WHERE type_statut = 4 AND date_expiration < NOW();
+                -- Marque les réservactions en attente expirées comme refusées
+                UPDATE reservations SET type_statut = 3 WHERE type_statut = 2 AND date_expiration < NOW();
+            END
+        ');
+
+        self::assertQuery('DROP PROCEDURE IF EXISTS FAST_REFRESH_AVAILABILITIES');
+        self::assertQuery('
             CREATE PROCEDURE FAST_REFRESH_AVAILABILITIES()
             BEGIN
+                -- Rend disponible les places de réservations actives expirées
                 UPDATE places SET disponible = true WHERE id IN (SELECT id_place FROM reservations WHERE type_statut = 4 AND date_expiration < NOW());
-                UPDATE reservations SET type_statut = 1 WHERE type_statut = 4 AND date_expiration < NOW();
-            END
-        '));
 
-        assert(DB::unprepared('DROP PROCEDURE IF EXISTS FULL_REFRESH_AVAILABILITIES'));
-        assert(DB::unprepared('
+                CALL REFRESH_AVAILABILITIES();
+            END
+        ');
+
+        self::assertQuery('DROP PROCEDURE IF EXISTS FULL_REFRESH_AVAILABILITIES');
+        self::assertQuery('
             CREATE PROCEDURE FULL_REFRESH_AVAILABILITIES()
             BEGIN
+                -- Rend toutes les places disponibles
                 UPDATE places SET disponible = true;
-                UPDATE reservations SET type_statut = 1 WHERE type_statut = 4 AND date_expiration < NOW();
+
+                CALL REFRESH_AVAILABILITIES();
+                -- Marque indisponibles les places faisant l\'objet d\'une réservation active
                 UPDATE places SET disponible = false WHERE id IN (SELECT id_place FROM reservations WHERE type_statut = 4);
             END
-        '));
+        ');
     }
 
     /**
@@ -42,7 +55,13 @@ class AddPsmThings extends Migration
      */
     public function down()
     {
-        assert(DB::unprepared('DROP PROCEDURE FAST_REFRESH_AVAILABILITIES'));
-        assert(DB::unprepared('DROP PROCEDURE FULL_REFRESH_AVAILABILITIES'));
+        self::assertQuery('DROP PROCEDURE REFRESH_AVAILABILITIES');
+        self::assertQuery('DROP PROCEDURE FAST_REFRESH_AVAILABILITIES');
+        self::assertQuery('DROP PROCEDURE FULL_REFRESH_AVAILABILITIES');
+    }
+
+    private static function assertQuery(string $query)
+    {
+        return assert(DB::unprepared($query));
     }
 }
